2cae99a08655ad4306859222de4673bfa304f659
mohammadsiyou <mohammadcj003@gmail.com>
2024-03-08 20:27:53 +0330

    Accomplish doctorsController

--- src/doctors/doctors.controller.spec.ts
+++ src/doctors/doctors.controller.spec.ts
@@ -5,6 +5,7 @@ import { Test } from '@nestjs/testing';
 import { AppointmentsService } from 'src/appointments/appointments.service';
 import { getDataSourceToken, getRepositoryToken } from '@nestjs/typeorm';
 import { CreateDoctorDto } from './dto/create-doctor.dto';
+import { Appointment } from 'src/appointments/appointment.entity';
 
 const createDoctorDto: CreateDoctorDto = {
     fullName: 'Doctor',
@@ -13,9 +14,12 @@ const createDoctorDto: CreateDoctorDto = {
 
 const doctors: Doctor[] = [{ id: '123', fullName: 'Doctor1', email: "doctor@doctor.com", appointments: [] }];
 
+const appointment: Appointment = { id: '111', startDate: new Date(), endDate: new Date(), isReserved: true, doctor: null, patient: null };
+
 describe('DoctorsController', () => {
     let doctorsController: DoctorsController;
     let doctorsService: DoctorsService;
+    let appointmentsService: AppointmentsService;
 
     beforeEach(async () => {
         const moduleRef = await Test.createTestingModule({
@@ -39,6 +43,15 @@ describe('DoctorsController', () => {
                 },
                 AppointmentsService,
                 {
+                    provide: AppointmentsService,
+                    useValue: {
+                        addApponitment: jest.fn(),
+                        getDoctorAppointments: jest.fn().mockImplementation((id: string) => Promise.resolve({ ...appointment, id })),
+                        getAppointment: jest.fn(),
+                        deleteAppointment: jest.fn(),
+                    }
+                },
+                {
                     // To mock datasource
                     provide: getDataSourceToken(),
                     useValue: {}
@@ -47,6 +60,7 @@ describe('DoctorsController', () => {
         }).compile();
 
         doctorsService = moduleRef.get<DoctorsService>(DoctorsService);
+        appointmentsService = moduleRef.get<AppointmentsService>(AppointmentsService);
         doctorsController = moduleRef.get<DoctorsController>(DoctorsController);
     });
 
@@ -91,6 +105,73 @@ describe('DoctorsController', () => {
             doctorsController.remove('1');
 
             expect(doctorsService.delete).toHaveBeenCalledTimes(1);
+        });
+    });
+
+    describe('addAppointments', () => {
+        it('should throw "endDate must be greater than startDate"', () => {
+            expect(doctorsController.addAppointments('', { startDate: new Date(), endDate: new Date(Date.now() - 1) }))
+                .rejects.toThrow('endDate must be greater than startDate')
+        });
+        it('should throw "Doctor not found"', () => {
+            jest.spyOn(doctorsController, 'findOne').mockImplementation(() => null);
+
+            expect(doctorsController.addAppointments('', { startDate: new Date(), endDate: new Date(Date.now()) }))
+                .rejects.toThrow('Doctor not found')
+        });
+        it('should return "Appointments added."', () => {
+            expect(doctorsController.addAppointments('', { startDate: new Date(), endDate: new Date(Date.now()) }))
+                .resolves.toBe('Appointments added.')
+        });
+        it('"addApponitment" should be called 3 times', async () => {
+            const startDate = new Date();
+            const endDate = new Date(Date.now() + 1800000 * 3);
+
+            await doctorsController.addAppointments('', { startDate, endDate });
+
+            expect(appointmentsService.addApponitment).toHaveBeenCalledTimes(3);
+        });
+    });
+
+    describe('getAppointments', () => {
+        it('should throw "Doctor not found"', () => {
+            jest.spyOn(doctorsController, 'findOne').mockImplementation(() => null);
+
+            expect(doctorsController.getAppointments('')).rejects.toThrow('Doctor not found')
+        });
+        it('should return an appointment', () => {
+            jest.spyOn(doctorsController, 'findOne').mockImplementation(() => Promise.resolve({ ...createDoctorDto, id: '111', appointments: [] }));
+
+            expect(doctorsController.getAppointments('111')).resolves.toEqual({ ...appointment, id: '111' });
+        });
+    });
+
+    describe('deleteAppointment', () => {
+        it('should throw "Doctor not found"', () => {
+            jest.spyOn(doctorsController, 'findOne').mockImplementation(() => null);
+
+            expect(doctorsController.deleteAppointment('', '')).rejects.toThrow('Doctor not found.')
+        });
+        it('should throw "Appointment not found."', () => {
+            jest.spyOn(doctorsController, 'findOne').mockImplementation(() => Promise.resolve(doctors[0]));
+
+            expect(doctorsController.deleteAppointment('', '')).rejects.toThrow('Appointment not found.')
+        });
+        it('should throw "The appointment has taken."', () => {
+            jest.spyOn(doctorsController, 'findOne').mockImplementation(() => Promise.resolve(doctors[0]));
+
+            jest.spyOn(appointmentsService, 'getAppointment').mockImplementation(() => Promise.resolve({ ...appointment, isReserved: true }));
+
+            expect(doctorsController.deleteAppointment('', '1')).rejects.toThrow('The appointment has taken.')
+        });
+        it('"deleteAppointment" should be called', async () => {
+            jest.spyOn(doctorsController, 'findOne').mockImplementation(() => Promise.resolve(doctors[0]));
+
+            jest.spyOn(appointmentsService, 'getAppointment').mockImplementation(() => Promise.resolve({ ...appointment, isReserved: false }));
+
+            await doctorsController.deleteAppointment('', '1');
+
+            expect(appointmentsService.deleteAppointment).toHaveBeenCalledTimes(1);
         });
     });
 });
